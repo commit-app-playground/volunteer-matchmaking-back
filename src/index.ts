ddb237b89b08c941f9b61bb5fe7189c929f7f08d
Rui Ni <rui@devrni.com>
2021-12-15 09:13:50 -0800

    refactor file structure to make things clean

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,29 @@ import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import express from 'express'
 import http from 'http'
-import resolvers from './graphql/resolvers';
-import typeDefs from './graphql/schemas';
+import schema from './graphql/schema'
 import depthLimit from 'graphql-depth-limit';
 import compression from "compression";
 
-async function listen(port: number) {
+async function startApolloServer(port: number) {
     const app = express()
-    app.use(compression());
-    
     const httpServer = http.createServer(app)
-
     const server = new ApolloServer({
-        typeDefs,
-        resolvers,
+        schema,
         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
         validationRules: [depthLimit(7)],
     })
     await server.start()
+    server.applyMiddleware({ app, ...compression })
 
-    server.applyMiddleware({ app })
-
-    return new Promise((resolve, reject) => {
-        httpServer.listen(port).once('listening', resolve).once('error', reject)
-    })
+    await new Promise<void>(resolve => httpServer.listen({ port }, resolve));
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
 }
 
 async function main() {
     const port = parseInt(config.port!);
     try {
-        await listen(port)
-        console.log(`Server is ready at http://localhost:${port}/graphql`)
+        startApolloServer(port)
     } catch (err) {
         console.error('Error starting the node server', err)
     }
